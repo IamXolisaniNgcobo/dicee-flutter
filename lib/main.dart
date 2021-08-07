e0e5c0ee10c62b56ff70c06d282d64110646d150
XOLISANI NGCOBO <38984822+IamXolisaniNgcobo@users.noreply.github.com>
2021-08-07 20:57:55 +0200

    update to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,37 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int left_dice_number = 1;
+  int right_dice_number = 0;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$left_dice_number.png'),
+              onPressed: () {
+                print('Left button pressed');
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice2.png'),
+              onPressed: () {
+                print('Right button pressed');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
